f75d4aef9fc409ced4c9b67d68fdeb1b98c9ad05
Anderson Zanichelli <andersonzanichelli@gmail.com>
2014-09-20 03:16:56 +0000

    adicionados os arquivos user.js e index.jade

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.send({'status': 'online'});
+});
+
+router.get('/:email', function(req, res) {
+    if(req.query.email === 'anderzanic@gmail.com') {
+        res.send({'status': true});
+    } else {
+        res.send({'status': false});
+    }
 });
 
 module.exports = router;
