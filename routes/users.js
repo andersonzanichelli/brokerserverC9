d96ce5b45b1bef3f75d9b44d3f12f69b9e914ec9
Anderson Zanichelli <andersonzanichelli@gmail.com>
2014-10-20 22:24:47 +0000

    adicionado funcionalidade add e del usuarios

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send({'status': 'online'});
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    db.collection('userlist').find().toArray(function (err, items) {
+        res.json(items);
+    });
 });
 
-router.get('/:email', function(req, res) {
-    if(req.query.email === 'anderzanic@gmail.com') {
-        res.send({'status': true});
-    } else {
-        res.send({'status': false});
-    }
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    db.collection('userlist').insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var userToDelete = req.params.id;
+    db.collection('userlist').removeById(userToDelete, function(err, result) {
+        res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+router.post('/:email', function(req, res) {
+    var db = req.db;
+    var email = {"email": req.query.email};
+    
+    db.collection('userlist').find(email).toArray(function (err, items) {
+        if(items.length > 0) {
+            res.send({'available': false});
+        } else {
+            res.send({'available': true});
+        }
+    });
 });
 
 module.exports = router;
